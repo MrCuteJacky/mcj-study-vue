80b1d5a5a61ba8241f396c0f98426ad0bd6c3e28
Jacky <mrcutejacky@gmail.com>
2021-02-02 11:39:06 +0800

    lab12, step2

--- lab12_vue3Component/app.js
+++ lab12_vue3Component/app.js
@@ -3,7 +3,7 @@ Vue.createApp({
         return {
             courses: [
                 {id: 'POOP', name: 'Python OOP Programming', duration: 35},
-                {id: 'BDPY', name: 'Python and bigdata', duration: 35}
+                {id: 'BDPY', name: 'Python and big data', duration: 35}
             ],
             detailsVisible: true
         }
