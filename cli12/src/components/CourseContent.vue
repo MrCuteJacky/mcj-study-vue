5b965472740558dc949d130d1083753187756dab
Jacky <mrcutejacky@gmail.com>
2021-02-02 15:12:34 +0800

    cli12

--- cli12/src/components/CourseContent.vue
+++ cli12/src/components/CourseContent.vue
@@ -25,7 +25,9 @@ export default {
   }
 }
 </script>
-
+h2 {
+  color: red;
+}
 <style scoped>
 
 </style>
--- cli12/src/main.js
+++ cli12/src/main.js
@@ -3,25 +3,6 @@ import App from './App.vue'
 
 const app = createApp(App)
 app.component('course-content', {
-    props: ['course'],
-    template: `
-        <li>
-            <h2>{{course.id}}</h2>
-            <button @click="toggleCourseDetail">show detail</button>
-            <ul v-if="detailsVisible">
-                <li>{{course.name}}</li>
-                <li>{{course.duration}}</li>
-            </ul>
-        </li>`,
-    data() {
-        return {
-            detailsVisible: true
-        }
-    },
-    methods: {
-        toggleCourseDetail() {
-            this.detailsVisible = !this.detailsVisible
-        }
-    }
+    App
 })
 app.mount('#app')
