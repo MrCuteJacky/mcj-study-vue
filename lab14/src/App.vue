916b53a5874eb7d0ae12fed4b10c5bbfbf03b4fc
Jacky <mrcutejacky@gmail.com>
2021-02-03 10:00:03 +0800

    lab14, add Composition1 component.

--- lab14/src/App.vue
+++ lab14/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Composition1></Composition1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Composition1 from "@/components/Composition1";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Composition1
   }
 }
 </script>
---
+++ lab14/src/components/Composition1.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h3>username={{username}}</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Composition1",
+  data() {
+    return {
+      username: "Jacky"
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
