ef94fb6aaed7e635a92495baa25fde506e13bc58
Jacky <mrcutejacky@gmail.com>
2021-02-03 10:29:22 +0800

    lab14, ref object.

--- lab14/src/components/Composition1.vue
+++ lab14/src/components/Composition1.vue
@@ -1,12 +1,16 @@
 <template>
   <section class="data">
     <h3>data</h3>
-    <h4>name: {{ username }}</h4>
+    <h4>name: {{ name }}</h4>
   </section>
   <section class="ref">
     <h3>ref</h3>
     <h4>Name: {{ currentName }}</h4>
     <h4>Age: {{ currentAge }}</h4>
+    <h4>user1.name: {{currentUser1.name}}</h4>
+    <h4>user1.age: {{currentUser1.age}}</h4>
+    <h4>user2.name: {{currentUser2.name}}</h4>
+    <h4>user2.age: {{currentUser2.age}}</h4>
   </section>
 </template>
 
@@ -18,17 +22,23 @@ export default {
   setup() {
     const currentName = ref("Jacky")
     const currentAge = ref(18)
+    let user1 = {name: "Mary", age: 19}
+    let user2 = ref({name: "Mary", age: 19})
     setTimeout(() => {
-      currentName.value = "Jacky tsai"
+      currentName.value = "Jacky(change)"
+      user1 = {name: "Mary(change)", age: 20}
+      user2.value = {name: "Mary(change)", age: 20}
     }, 3000)
     return {
       currentName: currentName,
-      currentAge: currentAge
+      currentAge: currentAge,
+      currentUser1: user1,
+      currentUser2: user2
     }
   },
   data() {
     return {
-      username: "Jacky"
+      name: "Jacky"
     }
   }
 }
