b3a9cab77008170d8da23e5dc5376d1d32a0a3e2
Jacky <mrcutejacky@gmail.com>
2021-02-03 10:18:30 +0800

    lab14, ref.

--- lab14/src/components/Composition1.vue
+++ lab14/src/components/Composition1.vue
@@ -1,12 +1,31 @@
 <template>
-  <section>
-    <h3>username={{username}}</h3>
+  <section class="data">
+    <h3>data</h3>
+    <h4>name: {{ username }}</h4>
+  </section>
+  <section class="ref">
+    <h3>ref</h3>
+    <h4>Name: {{ currentName }}</h4>
+    <h4>Age: {{ currentAge }}</h4>
   </section>
 </template>
 
 <script>
+import {ref} from "@vue/reactivity";
+
 export default {
   name: "Composition1",
+  setup() {
+    const currentName = ref("Jacky")
+    const currentAge = ref(18)
+    setTimeout(() => {
+      currentName.value = "Jacky tsai"
+    }, 3000)
+    return {
+      currentName: currentName,
+      currentAge: currentAge
+    }
+  },
   data() {
     return {
       username: "Jacky"
@@ -16,5 +35,16 @@ export default {
 </script>
 
 <style scoped>
-
+h3 {
+  color: darkred;
+}
+section {
+  padding: 0px 10px;
+}
+section.data {
+  background-color: mediumseagreen;
+}
+section.ref {
+  background-color: darkseagreen;
+}
 </style>
